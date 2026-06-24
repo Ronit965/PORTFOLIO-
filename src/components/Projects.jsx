0b06ca3b525cf4b436ef5e356d51efaa38e8ad63
ronitkumar965 <ronitkumar9582@gmail.com>
2026-06-24 12:22:36 +0530

    add django

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,7 +9,7 @@ const projects = [
         title: 'AI DESIGN GENERATOR',
         description:
             'Implemented a clean, minimalist UI/UX following AI DESIGN GENERATOR and aesthetic standards.',
-        tags: ['React', 'Css', 'Figma', 'Motion'],
+        tags: ['React', 'Django', 'Css', 'Figma', 'Motion'],
         github: 'https://github.com/Ronit965/Apple',   // GitHub repo
         live: 'https://ai-design-generator-ihij.vercel.app/',   // Live demo
         gradient: 'linear-gradient(135deg, rgba(191,64,191,0.15), rgba(0,245,255,0.05))',
